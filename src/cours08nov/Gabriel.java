0a462c1814a5fba66453b0981fdffecc1d4d6d25
Admin <Admin@DESKTOP-TLTDT9O>
2021-11-08 16:56:16 -0500

    Changer la variable nom pour: prénom et nom dans la classe Gabriel

--- src/cours08nov/Gabriel.java
+++ src/cours08nov/Gabriel.java
@@ -10,32 +10,7 @@ package cours08nov;
  * @author Admin
  */
 public class Gabriel {
-    
-    //variables
+    //Variables
+    private String prénom;
     private String nom;
-    private String prenom;
-
-    public Gabriel(String nom, String prenom) {
-        this.nom = nom;
-        this.prenom = prenom;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-    
-    
-    
 }
